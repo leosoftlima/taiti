47d956ff9ea5c37af6971b1084cc1e6ec9cf914e
leosoftlima <leonardo.souza.lima@gmail.com>
2021-03-17 19:54:01 -0300

    ajustando o javaUtil

--- src/main/groovy/br/ufpe/cin/tan/util/java/JavaUtil.groovy
+++ src/main/groovy/br/ufpe/cin/tan/util/java/JavaUtil.groovy
@@ -12,35 +12,11 @@ class JavaUtil extends Util {
         if (!path || path.empty || path.isAllWhitespace()) return ""
         def firstIndex = path.lastIndexOf(File.separator)
         def lastIndex = path.lastIndexOf(".")
-        def underscore = ""
-        if (firstIndex >= 0 && lastIndex >= 0 && firstIndex + 1 < lastIndex) underscore = path.substring(firstIndex + 1, lastIndex)
-        underscoreToCamelCase(underscore)
+        def name = ""
+        if (firstIndex >= 0 && lastIndex >= 0 && firstIndex + 1 < lastIndex) name = path.substring(firstIndex + 1, lastIndex)
+        else if (firstIndex==-1 && lastIndex>0) name = path.substring(0, lastIndex)
+        name
     }
-
-    static List<String> getClassPathForRubyClass(String original, Collection<String> projectFiles) {
-        def underscore = camelCaseToUnderscore(original)
-        getClassPathForVariable(underscore, projectFiles)
-    }
-    static List<String> getClassPathForVariable(String original, Collection<String> projectFiles) {
-        if (original.empty || original.contains(" ")) return [] //invalid class reference
-        def name = camelCaseToUnderscore(original) + ConstantData.JAVA_EXTENSION
-        def exp = ".*$File.separator$name".replace(File.separator, Matcher.quoteReplacement(File.separator))
-        projectFiles?.findAll { it ==~ /$exp/ }
-    }
-
-     static boolean isRouteMethod(String name) {
-        if (!(name in JavaConstantData.EXCLUDED_PATH_METHODS) && name.endsWith(JavaConstantData.ROUTE_PATH_SUFIX)) true
-        else false
-    }
-
-      private static  List<Node> getAllNodes(Node node) {
-	   	List<Node> nodes = new LinkedList<>();
-	    nodes.add(node);
-	    node.getChildNodes().each {children -> 
-	        nodes.addAll(getAllNodes(children));
-	    };
-	    return nodes;
-	} 
 
     static List<String> getClassPathForJavaClass(String original, Collection<String> projectFiles) {
         def name = original + ConstantData.JAVA_EXTENSION
